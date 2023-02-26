b6a267ccd2fdb211ca50129aa0274c517597cb23
Michał Sobiech (Laptop) <michal.sobiech15@gmail.com>
2023-02-26 14:12:22 +0100

    sample GET working

--- src/main/java/com/michal_sobiech/to_do_app/to_do_app/ToDoAppApplication.java
+++ src/main/java/com/michal_sobiech/to_do_app/to_do_app/ToDoAppApplication.java
@@ -3,11 +3,20 @@ package com.michal_sobiech.to_do_app.to_do_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class ToDoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public static String hello() {
+		return "sample text!";
 	}
 
 }
