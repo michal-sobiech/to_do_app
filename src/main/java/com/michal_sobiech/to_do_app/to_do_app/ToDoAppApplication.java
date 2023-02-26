4435131f1c83fa896b30360134ce9a0c53632c5c
Michał Sobiech (Laptop) <michal.sobiech15@gmail.com>
2023-02-26 13:19:17 +0100

    Spring Boot setup

---
+++ src/main/java/com/michal_sobiech/to_do_app/to_do_app/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.michal_sobiech.to_do_app.to_do_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
